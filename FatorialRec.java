37e02958e62f35fad893465bdf778341e5937067
Joelanio Menezes <joelanio@gmail.com>
2013-05-23 14:58:12 -0300

    Função fatorial recursiva em java

---
+++ FatorialRec.java
@@ -0,0 +1,14 @@
+public class FatorialRec {
+	
+	public static long fatorial(long n){
+		if(n==0)return 1;
+		else return n*fatorial(n-1);
+				
+	}
+	public static void main(String[] args) {
+		long n = Integer.parseInt(args[0]);
+		System.out.println("O fatorial de "+ n +" é: "+fatorial(n));
+		
+	}
+
+}
